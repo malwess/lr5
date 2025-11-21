ffc4f5dbc4a68c2c06d84d43b9c40d007ac3e351
Maria <yunda.masha2007@gmail.com>
2025-11-22 01:02:42 +0300

    result

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 def sum2(x, y):
     return x + y
+
+def ss4(x):
+    s = ''
+    while x > 0:
+        s = str(x % 4) + s
+        x = x // 4
+    return int(s)
+
+def polindrom(x):
+    if str(x)==str(x)[::-1]:
+        return True
+
+from fractions import Fraction
+
+def add_fractions(a: Fraction, b: Fraction) -> Fraction:
+    return a + b
+
+def multiply_fractions(a: Fraction, b: Fraction) -> Fraction:
+    return a * b
--- tests.py
+++ tests.py
@@ -1,4 +1,27 @@
-from main import sum2
+from main import *
 
 def test_sum2():
     assert sum2(15, 8) == 0
+
+def test_true():
+    assert True
+
+def test_message():
+    assert False, 'тест провален'
+
+def test_pass():
+    pass
+
+def test_ss4():
+    assert ss4(123) == 1323
+
+def test_polindrom():
+    assert polindrom(1221) == True
+
+def test_add_functions():
+    assert add_fractions(Fraction(1, 2), Fraction(1, 3)) == Fraction(5, 6)
+    assert add_fractions(Fraction(-1, 4), Fraction(1, 4)) == 0
+
+def test_multiply_fractions():
+    assert multiply_fractions(Fraction(2, 3), Fraction(3, 4)) == Fraction(1, 2)
+
